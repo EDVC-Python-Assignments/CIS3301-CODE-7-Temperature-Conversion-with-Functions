9c802bc6202677b5f2ab412f6cb4ff555a4d58c3
davicompu <villacis@vt.edu>
2023-02-09 16:42:48 -0700

    Update test_code_7.py

--- test_code_7.py
+++ test_code_7.py
@@ -2,7 +2,8 @@ import os,sys
 import random
 from mock_input_tests import *
 from code_7 import get_temperature_in_celsius
-from code_7 import get_temperature_in_kelvin
+from code_7 import get_temperature_in_kelvin_from_c
+from code_7 import get_temperature_in_kelvin_from_f
 from code_7 import get_clothing_recommendation
 from code_7 import cold_weather_clothing
 from code_7 import cool_weather_clothing
@@ -16,24 +17,31 @@ def check_if_file_exists():
     except:
         sys.exit()
 
-def test_conversion_farenheit_to_celcius():
+def test_conversion_farenheit_to_celsius():
     check_if_file_exists()
     random_temp_in_f = random.randint(32,70)
     temp_in_c = (random_temp_in_f-32)*0.5556
     assert temp_in_c == get_temperature_in_celsius(random_temp_in_f)
 
-def test_conversion_farenheit_to_kelvin():
+def test_conversion_celsius_to_kelvin():
     check_if_file_exists()
     random_temp_in_c = random.randint(0,40)
-    temp_in_c = random_temp_in_c + 273.15
-    assert temp_in_c == get_temperature_in_kelvin(random_temp_in_c)
+    temp_in_k = random_temp_in_c + 273.15
+    assert temp_in_k == get_temperature_in_kelvin_from_c(random_temp_in_c)
+
+def test_conversion_farenheit_to_kelvin():
+    check_if_file_exists()
+    random_temp_in_f = random.randint(0,40)
+    temp_in_c = (random_temp_in_f-32)*0.5556
+    temp_in_k = temp_in_c +273.15
+    assert temp_in_k == get_temperature_in_kelvin_from_f(random_temp_in_f)
 
 def test_clothing_recommendation():
     check_if_file_exists()
     cold_item = get_clothing_recommendation(random.randint(1,10))
     cool_item = get_clothing_recommendation(random.randint(11,15))
-    warm_item = get_clothing_recommendation(random.randint(16,24))
-    hot_item = get_clothing_recommendation(random.randint(25,35))
+    warm_item = get_clothing_recommendation(random.randint(16,25))
+    hot_item = get_clothing_recommendation(random.randint(26,35))
 
     assert cold_item in cold_weather_clothing
     assert cool_item in cool_weather_clothing
