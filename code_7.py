406b2fa4602ce622db86ad26f619cd83bad6fef9
davicompu <villacis@vt.edu>
2023-02-09 16:44:46 -0700

    Update code_7.py

--- code_7.py
+++ code_7.py
@@ -7,7 +7,10 @@ hot_weather_clothing = ["sun screen","water","shorts"]
 def get_temperature_in_celsius(temp_in_f):
    pass #Remove this line and insert your code HERE
 
-def get_temperature_in_kelvin(temp_in_c):
+def get_temperature_in_kelvin_from_c(temp_in_c):
+   pass #Remove this line and insert your code HERE
+
+def get_temperature_in_kelvin_from_f(temp_in_f):
    pass #Remove this line and insert your code HERE
 
 def get_clothing_recommendation(temp_in_c):
