e87b17535403cca0ab46aa758fad91b6c2af8a1e
davicompu <villacis@vt.edu>
2023-02-09 16:28:12 -0700

    Setup Initial template. Readme is missing

---
+++ code_7.py
@@ -0,0 +1,18 @@
+
+cold_weather_clothing = ["heavy jacket","scarf","hat"]
+cool_weather_clothing = ["jacket","sweater","gloves"]
+warm_weather_clothing = ["sun glasses","t-shirt","hat"]
+hot_weather_clothing = ["sun screen","water","shorts"]
+
+def get_temperature_in_celsius(temp_in_f):
+   pass
+
+def get_temperature_in_kelvin(temp_in_c):
+   pass
+
+def get_clothing_recommendation(temp_in_c):
+   pass
+   
+
+if __name__ == "__main__":
+   pass
---
+++ test_code_7.py
@@ -0,0 +1,42 @@
+import os,sys
+import random
+from mock_input_tests import *
+from code_7 import get_temperature_in_celsius
+from code_7 import get_temperature_in_kelvin
+from code_7 import get_clothing_recommendation
+from code_7 import cold_weather_clothing
+from code_7 import cool_weather_clothing
+from code_7 import warm_weather_clothing
+from code_7 import hot_weather_clothing
+
+def check_if_file_exists():
+    try:
+        exists = os.path.exists("code_7.py")
+        assert exists == True
+    except:
+        sys.exit()
+
+def test_conversion_farenheit_to_celcius():
+    check_if_file_exists()
+    random_temp_in_f = random.randint(32,70)
+    temp_in_c = (random_temp_in_f-32)*0.5556
+    assert temp_in_c == get_temperature_in_celsius(random_temp_in_f)
+
+def test_conversion_farenheit_to_kelvin():
+    check_if_file_exists()
+    random_temp_in_c = random.randint(0,40)
+    temp_in_c = random_temp_in_c + 273.15
+    assert temp_in_c == get_temperature_in_kelvin(random_temp_in_c)
+
+def test_clothing_recommendation():
+    check_if_file_exists()
+    cold_item = get_clothing_recommendation(random.randint(1,10))
+    cool_item = get_clothing_recommendation(random.randint(11,15))
+    warm_item = get_clothing_recommendation(random.randint(16,24))
+    hot_item = get_clothing_recommendation(random.randint(25,35))
+
+    assert cold_item in cold_weather_clothing
+    assert cool_item in cool_weather_clothing
+    assert warm_item in warm_weather_clothing
+    assert hot_item in hot_weather_clothing
+
